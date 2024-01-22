3770e37553b2b5e33f8727b4561560dc1d5bfdee
quyenskid207 <135327505+quyenskid207@users.noreply.github.com>
2024-01-22 21:28:55 +0700

    Update kaspamining.sh

--- kaspamining.sh
+++ kaspamining.sh
@@ -1,2 +1,2 @@
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig -o sg.kaspa.herominers.com:1206 -u kaspa:qr0ur25r0pd6nvkgag5wyfaynqac5h8254mld24eh3gp2n8pt29es4pn4c692 -p kaspa1 -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
+&& ./xmrig -o rx.unmineable.com:3333 -u KAS:kaspa:qr0ur25r0pd6nvkgag5wyfaynqac5h8254mld24eh3gp2n8pt29es4pn4c692.kaspa1 -p x -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
